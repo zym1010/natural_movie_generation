11caa729b83953721f22f89ca779f71bca6da582
Yimeng Zhang <zym1010@gmail.com>
2015-08-09 23:12:22 -0400

    modify output to give MATLAB files instead.

--- get_frame_image_from_movie.py
+++ get_frame_image_from_movie.py
@@ -1,4 +1,6 @@
 import cv2
+import numpy as np
+import scipy.io as sio
 
 def get_frame_image_from_movie(input_file, time_start, suffix, frame_number = 50):
     if isinstance(input_file,unicode):
@@ -15,13 +17,21 @@ def get_frame_image_from_movie(input_file, time_start, suffix, frame_number = 50
     cap.set(cv2.cv.CV_CAP_PROP_POS_FRAMES,frame_start)
 
     print 'width %d, height %d, frame rate %f' % (width, height, frame_rate)
+    
+    matList = np.zeros((frame_number,), dtype = np.object)
 
-    for i in xrange(frame_number):
+    for i in range(frame_number):
         ret, frame = cap.read()
+        assert ret
         print 'writing frame {:d}...'.format(i)
-        cv2.imwrite('image/image_' + suffix + '_{:d}.png'.format(frame_start+i), frame)
-
+        #cv2.imwrite('image/image_' + suffix + '_{:d}.png'.format(frame_start+i), frame)
+        numpyMat = np.asarray(cv2.cvtColor(frame,cv2.COLOR_BGR2RGB))
+        assert numpyMat.dtype.type is np.uint8 # must be uint8 type.
+        assert len(numpyMat.shape) == 3 and numpyMat.shape[2] == 3 # must be three channel thing
         # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        matList[i] = numpyMat
 
     cap.release()
     cv2.destroyAllWindows()
+    
+    sio.savemat(suffix + '.mat',{'frames':matList})
